a155abf92c4f44473fbe2be04b4d9e6afa6ecc01
tinatin92 <tinatygordadze@gmail.com>
2024-11-21 00:21:51 +0400

    solve versel error

--- src/pages/home/components/blog-item/index.tsx
+++ src/pages/home/components/blog-item/index.tsx
@@ -1,7 +1,7 @@
 import { Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Link } from "react-router-dom";
-import { useTranslation, Trans } from "react-i18next";
+import { useTranslation, /* Trans */ } from "react-i18next";
 
 type BlogItemProps = {
   title: string;
